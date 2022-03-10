9ea320dcae0e585bbfbc47d386d5ec3346087287
= <=>
2022-03-10 06:10:59 -0800

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
-
+/**
+ * main - This is main function
+ * Return: return 0 after reading the lines
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
