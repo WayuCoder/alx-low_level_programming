7fd466308136667d3077e238f4e94350b4edb164
= <=>
2022-03-10 06:20:51 -0800

    updated

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - main function of the programm
+ * Return: return value of the main function
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
