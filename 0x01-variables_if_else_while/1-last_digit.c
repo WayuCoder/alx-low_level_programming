2ef7fa1b4a67314c9571981090e95a56e684d20d
= <=>
2022-03-11 08:47:22 -0800

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 }
 else if (lastDigit > 5)
 {
-printf("Last Digit of %d is %d and is greater than 5\n", n, lastDigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else if (lastDigit < 6)
 {
-printf("Last digit of %d is %d less than 6 and not 0\n", n, lastDigit);
+printf("Last digit of %d is %n and is less than 6 and not 0\n", n, lastDigit);
 }
 return (0);
 }
