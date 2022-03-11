641907d7ea0c3a69f0c9ea9e52d67ed2df75b0c5
= <=>
2022-03-11 08:44:38 -0800

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 if (lastDigit == 0)
 {
-printf("The Last digit of %d is %d and is 0\n", n, lastDigit);
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 }
 else if (lastDigit > 5)
 {
-printf("The Last Digit of %d is %d and is greater than 5\n", n, lastDigit);
+printf("Last Digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
-else if (lastDigit > 0 && lastDigit < 6)
+else if (lastDigit < 6)
 {
-printf("The Last digit of %d is %d less than 6 and not 0\n", n, lastDigit);
+printf("Last digit of %d is %d less than 6 and not 0\n", n, lastDigit);
 }
 return (0);
 }
