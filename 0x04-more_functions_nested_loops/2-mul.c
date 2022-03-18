561758d824483c0324ffacebb9041f99d0175c04
= <=>
2022-03-18 06:48:39 -0700

    multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#includ "main.h"
+/**
+ * mul - check the code for Holberton School students.
+ * @a: character a
+ * @b: character b
+ * Return: success
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
