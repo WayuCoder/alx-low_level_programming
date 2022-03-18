8556c57e23017df04c1c2670630484e637ed56cc
= <=>
2022-03-18 07:02:37 -0700

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - check the code for Holberton School students.
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar('0' + i);
+}
+_putchar('\n');
+}
