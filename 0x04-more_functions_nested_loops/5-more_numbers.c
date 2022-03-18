f02aa1816c489b2746a2dd69e05158ef478c8cd7
= <=>
2022-03-18 07:41:13 -0700

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - check the code for Holberton School students
+ * Return: success
+ */
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0 ; j <= 14; j++)
+{
+if (j >= 10)
+_putchar('0' + (j / 10));
+_putchar('0' + (j % 10));
+}
+_putchar('\n');
+}
+return;
+}
