13cddc7163b3cf2ad4314f13afc210322a5143d0
= <=>
2022-03-21 07:05:15 -0700

    a function which prints a string followed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,19 @@
 #include "main.h"
-
 /**
- * main - check the code for Holberton School students.
- *
+ * _puts - check the code for Holberton School students.
+ * @str: integer1
  * Return: Always 0.
  */
-int main(void)
+void _puts(char *str)
 {
-char *str;
-
-str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-_puts(str);
-  return (0);
+char temp;
+int length = 0;
+while (*str != '\0')
+{
+temp = *str;
+_putchar(temp);
+length++;
+str++;
+}
+_putchar('\n');
 }
