170606cdcf69461f69ce7f95681eeceb951bd620
= <=>
2022-04-15 00:10:48 -0700

    betty style updated

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,16 +11,16 @@
 
 int main(int argc, char *argv[])
 {
-  int R;
+int R;
 
-  if (argc != 4)
-    {
-      printf("Error\n");
-      exit(98);
-    }
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-  R = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
-  printf("%d\n", R);
+R = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+printf("%d\n", R);
 
 return (0);
 }
